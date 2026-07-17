968feab9d8cb389afcd23d928a8ce3565fee8b30
halliburtonjb49-collab <halliburtonjb49@gmail.com>
2026-07-17 10:39:20 -0500

    Finalize branded login layout

--- lib/theme/prop_intelligence_colors.dart
+++ lib/theme/prop_intelligence_colors.dart
@@ -4,6 +4,6 @@ class PropIntelligenceColors {
   // Brand palette sourced from the logo treatment.
   static const Color darkCanvasBg = Color(0xFF060B11);
   static const Color darkCardBg = Color(0xFF0F1622);
-  static const Color premiumGold = Color(0xFFF3C344);
+  static const Color premiumGold = Color(0xFFFFC400);
   static const Color metallicSilver = Color(0xFFE0E0E0);
 }
